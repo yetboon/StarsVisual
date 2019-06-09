db75cb74b63c60957a38e8f3c960de96926c879f
yetboon <yetboontan@gmail.com>
2019-06-09 10:05:40 +0800

    Initial commit

---
+++ StarsVisual/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace StarsVisual
+{
+	class Program
+	{
+		
+		// 变量
+		static int starsLimit = 5;
+		
+		// 主程式
+		public static void Main(string[] args)
+		{
+			for (int i = 1; i < starsLimit + 1; i++) 
+			{
+				DrawStars(i);
+				Console.WriteLine();
+			}
+			
+			// 将 /* */ 给去除可使用里面的代码.
+			/*for (int i = starsLimit; i > 0; i--) 
+			{
+				DrawStars(i);
+				Console.WriteLine();
+			}*/
+			
+			
+			Console.ReadKey();
+		}
+		
+		
+		public static void DrawStars(int count)
+		{
+			for (int i = 0; i < count; i++) 
+			{
+				Console.Write("*");
+			}
+		}
+		
+	}
+}
